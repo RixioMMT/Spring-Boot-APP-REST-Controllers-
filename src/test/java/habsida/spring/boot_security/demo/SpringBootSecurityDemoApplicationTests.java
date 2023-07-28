6ce90798165b9805e256c982721ac5eb80b6875a
Alyona275 <alena.kim.220795@gmail.com>
2023-07-28 12:35:28 +0900

    initial commit

---
+++ src/test/java/habsida/spring/boot_security/demo/SpringBootSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package habsida.spring.boot_security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
